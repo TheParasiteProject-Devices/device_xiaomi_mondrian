053ebcd2094fa7d707750ec2379944b0aedd137d
someone5678 <someone5678@users.noreply.github.com>
2024-07-25 00:37:36 +0900

    mondrian: Parasitizing
    Change-Id: I7df083c5edfdcd426166450bdbc3f554daa6dbfb

--- aosp_mondrian.mk
+++ aosp_mondrian.mk
@@ -7,11 +7,26 @@
 # Inherit from mondrian device
 $(call inherit-product, device/xiaomi/mondrian/device.mk)
 
-# Inherit from common lineage configuration
+# Inherit from common TheParasiteProject configuration.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_CARRIER_SERVICES := true
+TARGET_INCLUDE_CARRIER_SETTINGS := true
+TARGET_SUPPORTS_GOOGLE_BATTERY := false
+TARGET_INCLUDE_CAMERA_GO := true
+TARGET_SUPPORTS_LILY_EXPERIENCE := true
+TARGET_GBOARD_KEY_HEIGHT := 1.2
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_mondrian
+TARGET_BOOT_ANIMATION_RES := 1440
+
+TARGET_USES_AOSP_RECOVERY := true
+
+PRODUCT_NAME := aosp_mondrian
 PRODUCT_DEVICE := mondrian
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -27,4 +42,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := POCO/mondrian_global/mondrian:14/UKQ1.230804.001/V816.0.5.0.UMNMIXM:user/release-keys
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_GMS_CLIENTID_BASE := android-google
